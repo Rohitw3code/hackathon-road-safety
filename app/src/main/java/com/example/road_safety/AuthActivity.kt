8050d550e6d547f73cb2307e7522d1b0f5a1201f
rohitw3code <rohitcode005@gmail.com>
2023-03-20 03:31:02 +0530

    upppd

--- app/src/main/java/com/example/road_safety/AuthActivity.kt
+++ app/src/main/java/com/example/road_safety/AuthActivity.kt
@@ -1,11 +1,63 @@
 package com.example.road_safety
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class AuthActivity : AppCompatActivity() {
+    lateinit var mGoogleSignInClient: GoogleSignInClient
+    var db = Firebase.firestore
+    val Req_Code: Int = 123
+    private lateinit var firebaseAuth: FirebaseAuth
+    var userRef = db.collection("users")
+    var adminRef = db.collection("admin")
+    private lateinit var authBtn:TextView
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+
+        authBtn = findViewById(R.id.auth_btn)
+
+        FirebaseApp.initializeApp(this)
+
+        authBtn = findViewById(R.id.auth_btn)
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        if (firebaseAuth.currentUser != null) {
+            startActivity(
+                Intent(
+                    this, MainActivity
+                    ::class.java
+                )
+            )
+            finish()
+        }
+        authBtn.setOnClickListener { view: View? ->
+//            Toast.makeText(this, "Logging In", Toast.LENGTH_SHORT).show()
+            signInGoogle()
+        }
+    }
+
+    private fun signInGoogle() {
+        val signInIntent: Intent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent, Req_Code)
     }
 }
