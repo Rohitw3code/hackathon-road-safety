ad2b2be7e7a70ee083bc24842b405904b151b266
Priyanshu Kushwaha <kushwaha.priyanshu.imp@gmail.com>
2023-03-20 21:01:00 +0530

    ok v10

--- app/src/main/java/com/example/road_safety/aboutActivity.kt
+++ app/src/main/java/com/example/road_safety/aboutActivity.kt
@@ -3,7 +3,7 @@ package com.example.road_safety
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class about_Activity : AppCompatActivity() {
+class aboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
